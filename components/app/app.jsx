fd19dadfca21893717824b9e409c32b311036924
rattvol <ratt_vol@rambler.ru>
2024-07-03 02:28:23 +0300

    Additional Part

--- components/app/app.jsx
+++ components/app/app.jsx
@@ -1,11 +1,34 @@
 import { Layout } from "../layout/layout";
 import { restaurants } from "../../materials/mock"
 import { Restaurant } from "../restaurant/restaurant";
+import { useState } from "react";
 
-export const App= ()=> {
-    return (
-        <Layout>
-            {restaurants.map(({id,name, menu,reviews}) =>  <Restaurant key={id} name = {name} menu={menu} reviews = {reviews} />)}
+const tabHandler = () => {
+    const [tabNum,setCount] = useState(0);
+
+    const setNew = (event) => {
+        setCount(Number(event.target.id)-1);
+    }
+
+    return {tabNum,setNew}
+    };
+
+
+    
+    export const App= ()=> {
+        const {tabNum,setNew} = tabHandler();
+        var num = 0;
+        return (
+            <Layout>
+             <div>
+                {restaurants.map(({name})=> { 
+                    num++;
+                    return ( <button className= "tabButton" id = {num} onClick={setNew}>{name}</button>); 
+                    })}
+                <div id="content">
+                    <Restaurant name = {restaurants[tabNum].name} menu={restaurants[tabNum].menu} reviews = {restaurants[tabNum].reviews} />
+                </div>
+            </div>
      </Layout>
    );
 }
