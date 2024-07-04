4e01be493a03c292ebf95cd9526fc232bede1000
rattvol <ratt_vol@rambler.ru>
2024-07-05 02:25:39 +0300

    Review fix

--- components/app/app.jsx
+++ components/app/app.jsx
@@ -1,34 +1,42 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { Layout } from "../layout/layout";
-import { restaurants } from "../../materials/mock"
+import { restaurants } from "../../materials/mock";
 import { Restaurant } from "../restaurant/restaurant";
 import { useState } from "react";
 
-const tabHandler = () => {
-    const [tabNum,setCount] = useState(0);
+const useTab = () => {
+  const [activeRestaurantId, setActiveRestaurantId] = useState(
+    restaurants[0].id
+  );
 
-    const setNew = (event) => {
-        setCount(Number(event.target.id)-1);
-    }
+  return { activeRestaurantId, setActiveRestaurantId };
+};
 
-    return {tabNum,setNew}
-    };
+export const App = () => {
+  const { activeRestaurantId, setActiveRestaurantId } = useTab();
 
+  const activeRestaurant = restaurants.find(
+    (restaurant) => restaurant.id === activeRestaurantId
+  );
 
-    
-    export const App= ()=> {
-        const {tabNum,setNew} = tabHandler();
-        var num = 0;
-        return (
-            <Layout>
-             <div>
-                {restaurants.map(({name})=> { 
-                    num++;
-                    return ( <button className= "tabButton" id = {num} onClick={setNew}>{name}</button>); 
-                    })}
-                <div id="content">
-                    <Restaurant name = {restaurants[tabNum].name} menu={restaurants[tabNum].menu} reviews = {restaurants[tabNum].reviews} />
-                </div>
-            </div>
-     </Layout>
-   );
-}
+  return (
+    <Layout>
+      <div>
+        {restaurants.map(({ id, name }) => {
+          return (
+            <button
+              key={id}
+              className="tabButton"
+              onClick={() => setActiveRestaurantId(id)}
+            >
+              {name}
+            </button>
+          );
+        })}
+        <div id="content">
+          {activeRestaurant && <Restaurant restaurant={activeRestaurant} />}
+        </div>
+      </div>
+    </Layout>
+  );
+};
